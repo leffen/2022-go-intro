6976294a886f09f02a2fe01aa52ac82e08fa2864
leffen <leffen@gmail.com>
2022-01-16 23:19:11 +0100

    Extending hello world

--- dag1/01-hello-world/hello.go
+++ dag1/01-hello-world/hello.go
@@ -1,7 +1,20 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+)
 
 func main() {
-	fmt.Printf("Hello world\n")
+	var num int = 100
+	var name string = "Ole Brum"
+
+	// Utskrift av linje med parametre
+	fmt.Printf("Hello %s %d\n", name, num)
+
+	fmt.Println("Test av utskrift av linjer")
+
+	// Utskrift til stderr
+	fmt.Fprintf(os.Stderr, "Test av utskrift til stderr. Name=%s\n", name)
+
 }
